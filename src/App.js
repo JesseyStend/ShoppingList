00e49f47ffe8228b7791611dda79481c43989f1f
JesseyStend <thejes@hotmail.nl>
2020-06-22 15:24:32 +0200

    ShoppingList v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      {
+        id: 0,
+        value: 0,
+      },
+      {
+        id: 1,
+        value: 0,
+      },
+      {
+        id: 2,
+        value: 0,
+      },
+      {
+        id: 3,
+        value: 9,
+      },
+    ],
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({
+      counters,
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter((c) => c.value > 0).length} />
+        <main className="container">
+          <ShoppingList list={this.state.counters} />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
